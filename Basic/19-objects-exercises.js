8a4fd4d0bfc1c579d7df96ead1c4fb7ff5f42c0c
AnsAjxup <aajxuph@miumg.edu.gt>
2025-12-06 11:30:10 -0600

    19-objects-exercises terminado

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -4,21 +4,51 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=15675
 */
 
 // 1. Crea un objeto con 3 propiedades
+let mascota = {
+    name: "Max",
+    raza: "Husky",
+    color: "Black & white"
+}
 
 // 2. Accede y muestra su valor
+console.log(mascota)
 
 // 3. Agrega una nueva propiedad
+mascota.colorOjos = "blue"
+console.log(mascota)
 
 // 4. Elimina una de las 3 primeras propiedades
+delete mascota.color
+console.log(mascota)
 
 // 5. Agrega una función e invócala
+mascota.saludar = function(){
+    console.log("El perro ladra")
+}
+
+mascota.saludar()
 
 // 6. Itera las propiedades del objeto
+for (let key in mascota){
+    console.log(`${key}: ${mascota[key]}`)
+}
 
 // 7. Crea un objeto anidado
+let vehiculo = {
+    marca: "toyota",
+    color: "negro",
+    conductor: {
+        nombre: "Pepito",
+        edad: 19,
+        licencia: true
+    }
+}
 
 // 8. Accede y muestra el valor de las propiedades anidadas
+console.log(vehiculo.conductor)
 
 // 9. Comprueba si los dos objetos creados son iguales
+console.log(mascota === vehiculo)
 
 // 10. Comprueba si dos propiedades diferentes son iguales
+console.log(mascota.name === vehiculo.conductor.nombre)
