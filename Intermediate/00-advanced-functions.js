a2f4c730b49566f2c95d0766ec9a0938cd569d6b
AnsAjxup <aajxuph@miumg.edu.gt>
2025-12-17 23:56:34 -0600

    notas de la clase

--- Intermediate/00-advanced-functions.js
+++ Intermediate/00-advanced-functions.js
@@ -25,11 +25,26 @@ greet2("Brais Moure")
 
 // Arrow functions avanzadas
 
-// - Retorno implícito
+// - Retorno implícito: 
+// se da cuando el cuerpo de la funcion es solo una expresión,
+// entonces el return es implicito y no necesitamos poner la
+// palabra reservada return
 const multiply = (a, b) => a * b
 console.log(multiply(2, 5))
 
 // - this léxico
+/* las funciones tienen la capacidad de entender el contexto de
+   las variable this en los objetos y clases. Por lo que no
+   tendriamos ningun inconveniente al usar this, esto lo podemos
+   ver representado de mejor manera en los metodos(funciones)
+   
+   Las Arrow Functions NO reconocen el contexto de this, por el 
+   contrario, crean su propio contexto. Esto quiere decir que
+   NO reconocen las variables this de los objetos o clases.
+
+   NOTA: Hay que tener cuidado al usar funciones flecha
+   en objetos y clases*/
+
 const handler = {
     name: "Brais",
     greeting: function () {
@@ -44,6 +59,19 @@ handler.greeting()
 handler.arrowGreeting();
 
 // IIFE (Expresión de Función Invocada Inmediatamente)
+/* Una funcion que se ejecutan en el momento en que se definan,
+    estas funciones se invocan inmediatamente al estar definidas,
+    es practimente una funcion que se ejecuta automaticamente.
+    Sin embargo, NO es habitual usarlas
+    
+    NOTA: Hay que delimitar bien las IIFE para que funcionen 
+    correctamente.
+
+    Sintaxis:
+    La funcion se encierra en parentecis y se ejecuta con otros
+    parentecis al final
+    (funcion)()
+    */
 
 (function () {
     console.log("IIFE clásico")
@@ -54,6 +82,10 @@ handler.arrowGreeting();
 })();
 
 // Parámetros Rest (...)
+/*  Se suele usar cuando se desconoce la cantidad de argumentos
+    que la funcion va a utilizar, estos argumentos terminan
+    conformando un array y se pasa como parametro.
+    Practiacamente empaqueta los parametros*/
 
 function sum(...numbers) {
     let result = 0
@@ -67,6 +99,11 @@ console.log(sum(1, 2, 3, 4, 5))
 console.log(sum(10, 15))
 
 // Operador Spread (...)
+/*  Se utiliza para desempaquetar parametros.
+    Normalmente se utiliza para pasar un array como parametros,
+    pero la cantidad de valores del array debe coincidir con la
+    cantidad de parametros de la función, de lo contrario podriamos
+    generar errores */
 
 const numbers = [1, 2, 3]
 function sumWithSpread(a, b, c) {
@@ -77,7 +114,12 @@ console.log(sumWithSpread(1, 2, 3)) // Sin Spread
 console.log(sumWithSpread(...numbers)) // Con Spread
 
 // Closures (Clausuras)
-
+/*  Es cuando una funcion que es interna a otra función 
+    accede a varibles de la funcion externa.
+    Una de sus principales caracteristicas es que permite
+    guardar el estado de las variables globales de la funcion
+    principal.
+ */
 function createCounter() {
     let counter = 0
     return function () {
@@ -93,6 +135,10 @@ counter()
 counter()
 
 // Recursividad
+/*  Es la capacidad que tiene la funcion de llamarse a ella misma
+    bajo un criterio especifico que delimite su contexto donde en 
+    algun momento deje de llamarse a si misma.
+ */
 
 function factorial(n) {
     if (n <= 1) {
@@ -104,6 +150,11 @@ function factorial(n) {
 console.log(factorial(5))
 
 // Funciones parciales
+/*  Es una técnica de programación funcional, donde creamos nuevas
+funciones más especificas a partir de una existente. Esto se hace
+en base a los argumentos para reducir la cantidad de parametros
+necesarios. 
+*/
 
 function partialSum(a) {
     return function (b, c) {
@@ -116,6 +167,9 @@ console.log(sumWith(2, 3))
 console.log(sumWith(1, 2))
 
 // Currying
+/*Transformar una funcion que recive varios parametros
+en una secuencia de funciones anidadas que reciben un solo
+parametro a la vez*/
 
 function currySum(a) {
     return function (b) {
@@ -134,6 +188,8 @@ console.log(sumC(4))
 console.log(sumAB(5)(7))
 
 // Callbacks
+/*  Es una función que se pasa como argumento a otra funcion
+para ser ejecutada despues, se suele usar en tareas asíncronas*/
 
 function processData(data, callback) {
     const result = sum(...data)
