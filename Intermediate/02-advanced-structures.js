7e0e7a4ee0ab1ea75b51074037b8045dad7bace7
AnsAjxup <aajxuph@miumg.edu.gt>
2025-12-20 19:13:30 -0600

    notas de clase

--- Intermediate/02-advanced-structures.js
+++ Intermediate/02-advanced-structures.js
@@ -8,29 +8,53 @@ Vídeo: https://youtu.be/iJvLAZ8MJ2E?t=4355
 // - Métodos funcionales
 
 // forEach
+/**
+ * Nos sirve para ejecutar una funcián por cada elemento
+ * de un array
+ */
 
 let numbers = [1, 2, 3, 4, 5, 6]
 
 numbers.forEach(element => console.log(element))
 
 // map
-
+/**
+ * Ejecuta una función por cada elemento del array
+ * y devuelve un nuevo array con los cambios
+ */
 let doubled = numbers.map(element => element * 2)
 console.log(doubled)
 
 // filter
+/**
+ * Filtra elementos y devuelve un nuevo array con los
+ * elementos que cumplan la condición
+ */
 
 let evens = numbers.filter(element => element % 2 === 0)
 console.log(evens)
 
 // reduce
+/**
+ * Este método reduce todos los elementos de un array a un 
+ * solo valos, aplicando una función acumuladora
+ * Ejemplo: suma todos los numeros de un array
+ * result -> es el acumulador
+ * current -> es el elemento acutal
+ * 0 -> es el valor inicial, se recomiendo ponerlo para evitar errores
+ */
 
 let sum = numbers.reduce((result, current) => result + current, 0)
 console.log(sum)
 
+
 // - Manipulación
 
 // flat
+/**
+ * Aplana un array a una profundidad especifica y retorna un
+ * nuevo array
+ */
 
 let nestedArray = [1, [2, [3, [4]]]]
 console.log(nestedArray)
@@ -42,14 +66,26 @@ flatArray = nestedArray.flat(3)
 console.log(flatArray)
 
 // flatMap
+/**
+ * combina map() + flat() en una sola operación.
+ * Primero transforma cada elemento y luego aplana el resultado 
+ * en un nivel.
+ */
 
 let phrases = ["Hola mundo", "Adiós mundo"]
 let words = phrases.flatMap(phrase => phrase.split(" "))
 console.log(words)
 
+
 // - Ordenación
 
 // sort
+/**
+ * Sirve para ordenar elementos de un array, por defecto
+ * funciona con strings, pero para ordenar numeros debemos
+ * indicarle cierto criterio de ordenamiento.
+ * Este método devuelve un nuevo array ordenado
+ */
 
 let unsorted = ["b", "a", "d", "c"]
 let sorted = unsorted.sort()
@@ -61,26 +97,41 @@ sorted = unsorted.sort((a, b) => a - b)
 console.log(sorted)
 
 // reverse
+/**
+ * Invierte el orden de los elementos de un array
+ * Este método muta el array, modifica el array original
+ */
 
 sorted.reverse()
 console.log(sorted)
 
+
 // - Búsqueda
 
 // includes
+/**Sirve para comprobar si un valor exite dentro del array
+ * este método devuelve un booleano (true o false)
+ */
 
 console.log(sorted.includes(4))
 console.log(sorted.includes(5))
 
 // find
+/**Sirve para encontrar el PRIMER elemento de un array que
+ * cumpla una condición. Si no encuenta nada devuelve undefined.
+ */
 
 let firstEven = sorted.find(element => element % 2 === 0)
 console.log(firstEven)
 
 // findIndex
+/**Similar a find pero en este caso devuelve el indice, si no 
+ * encuentra el elemento retorna -1 como indice
+ */
 
 let firstEvenIndex = sorted.findIndex(element => element % 2 === 0)
 console.log(firstEvenIndex)
+
 
 // Sets avanzados
 
@@ -92,6 +143,7 @@ let numbersArray = [1, 2, 2, 3, 4, 5, 6, 6]
 numbersArray = [...new Set(numbersArray)]
 console.log(numbersArray)
 
+//Los sets son perfectos para trabajar con conjuntos
 // Unión
 
 const setA = new Set([1, 2, 3])
@@ -109,17 +161,20 @@ console.log(intersection)
 const difference = new Set([...setA].filter(element => !setB.has(element)))
 console.log(difference)
 
+
 // - Conversión
 
 // Set a Array
 
 console.log([...setA])
 
+
 // - Iteración
 
 // forEach
 
 setA.forEach(element => console.log(element))
+
 
 // Maps avanzados
 
